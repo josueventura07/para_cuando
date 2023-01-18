395e2b71b3f56d01bda48eaf8f058f0f1d45de21
Mems2001 <marioed95@hotmail.com>
2023-01-18 12:04:51 -0500

    fix: role middleware fiexed (Mario)

--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -1,9 +1,20 @@
-const roleMiddleware = (req , res , next) => {
-  if (req.user.role === 'admin') {
-    next()
-  } else {
-    res.status(401).json({
-      message: 'Permission denied'
+const { verifyAdmin } = require('../controllers/profiles.controllers')
+
+const roleMiddleware = async(req , res , next) => {
+  try {
+    const admin = await verifyAdmin(req.user.id)
+    // console.log(admin)
+
+    if (admin) {
+      next()
+    } else {
+      res.status(401).json({
+        message: 'Permission denied'
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
     })
   }
 }
